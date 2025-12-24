65548a6e2e4801e8883336379849ee7a04c650a2
KurokawaMasaya <83994513+KurokawaMasaya@users.noreply.github.com>
2025-12-24 00:47:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ LANG_DICT = {
         "egg_padoru": "🎵 走れ逸れよ... 風のように... PADORU PADORU !!! 🧣",
         "egg_snow": "❄️ 讓雪落下吧！整個世界都安靜了...",
         "egg_market": "🍷 歡迎來到聖誕集市！來杯熱紅酒配扭結餅吧！🥨",
-        "egg_author": "👨‍💻 作者出現！致敬時刻...",
+        "egg_author": "👨‍💻 作者出現！致敬時刻一人一句qyc nb!!",
         "hint_prefix": "💡 **解鎖新謎題：** ",
         "final_hint_title": "🔒 最終封印已解除",
         "final_hint_msg": "🎅 **吼吼吼，本聖誕老人找到了一份來自中國的寶貝，真是諷刺啊。**\n\n想看嗎？想看請輸入 **聖誕快樂**。"
@@ -102,7 +102,7 @@ LANG_DICT = {
         "egg_padoru": "🎵 走れ逸れよ... 風のように... PADORU PADORU !!! 🧣",
         "egg_snow": "❄️ 让雪落下吧！整个世界都安静了...",
         "egg_market": "🍷 欢迎来到圣诞集市！来杯热红酒配扭结饼吧！🥨",
-        "egg_author": "👨‍💻 作者出现！致敬时刻...",
+        "egg_author": "👨‍💻 作者出现！致敬时刻，一人一句qyc nb！",
         "hint_prefix": "💡 **解锁新谜题：** ",
         "final_hint_title": "🔒 最终封印已解除",
         "final_hint_msg": "🎅 **吼吼吼，本圣诞老人找到了一份来自中国大陆的宝贝。**\n\n请输入 **圣诞快乐**查看。"
@@ -877,7 +877,7 @@ else:
 
             elif any(t in user_input_lower for t in triggers_author):
                 st.balloons()
-                st.image("pic.png", caption="The Creator", width=1000)
+                st.image("pic.png", caption="The Creator", width=900)
                 st.markdown(f"<div class='roast-box gold-mode' style='border-left: 5px solid #4CAF50 !important;'>{ui_text['egg_author']}</div>", unsafe_allow_html=True)
 
             else:
